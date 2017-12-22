535264990e55a08015d621ee9c1bf13eaf49087f
Joshua Gonzales <niputen13@gmail.com>
2017-12-22 22:42:48 +0800

    done

--- public/backend.js
+++ public/backend.js
@@ -1,36 +1,27 @@
-
-$(document).ready(function(){
-
+//=======================================ADDING NEW RECORDS=========================================================
+$(document).ready(function()
+{
 	var name = $('.user-name').val();
 	var phone = $('.user-number').val();
-
 	localStorage.setItem('name',name);
 	localStorage.setItem('phone',phone);
-
-	$('.submit-btn').click(function(){
-
-		$.post('https://joshphonebook.herokuapp.com/new',{
-
+	$('.submit-btn').click(function()
+	{
+		$.post('https://joshphonebook.herokuapp.com/new',
+		{
 			number: $('.user-number').val(),
 			name:$('.user-name').val()
-		}).done(function(res){
+		}).done(function(res)
+		{
 				console.log(res);
 				 $("#wew tr").remove();
 				 getrecords();
 				$('.user-name').val("");
 				$('.user-number').val("");
 		});
-
 	});
-
-
-	
-
-
-
 });
-
-
+//========================================GETTING RECORDS FROM DATABASE USING API====================================
 function getrecords()
 {
 var uid;
@@ -40,71 +31,64 @@ var count=0;
 	$.get('http://joshphonebook.herokuapp.com/records',function(data)
 						        {
 						          console.log(data);
- 									$.each(data.data, function(i, d) {
- 									uid = data.data[count].id;
- 									uname = data.data[count].name;
- 									uphone = data.data[count].number;
-          							count++;
-						             var row='<tr>';
-						             $.each(d, function(j, e) {
-						                row+='<td>'+e+'</td>';
-
-						             });
-						              row+='<td> <button type="button" class = " glyphicon glyphicon-edit btn btn-primary" onclick="edit(\'' + uid + '\',\'' + uphone + '\',\'' + uname + '\');"> EDIT </button> <button type="button" class="btn btn-warning glyphicon glyphicon-remove" onclick="remove(\'' + uid + '\');"> Delete </button></td>';
-						             row+='</tr>';
-						             $('#wew').append(row);
-						          });
-						           $('#wew').DataTable();
+ 									$.each(data.data, function(i, d) 
+ 									{
+	 									uid = data.data[count].id;
+	 									uname = data.data[count].name;
+	 									uphone = data.data[count].number;
+	          							count++;
+							             var row='<tr>';
+							             $.each(d, function(j, e) 
+							             {
+							                row+='<td>'+e+'</td>';
+						             	 });
+							             row+='<td> <button type="button" class = " glyphicon glyphicon-edit btn btn-primary" onclick="edit(\'' + uid + '\',\'' + uphone + '\',\'' + uname + '\');"> EDIT </button> <button type="button" class="btn btn-warning glyphicon glyphicon-remove" onclick="remove(\'' + uid + '\');"> Delete </button></td>';
+							             row+='</tr>';
+							             $('#wew').append(row);
+						            });
+						           		 $('#wew').DataTable();
 						          	
 						        });
-
-
 }
+//==============================================EDITING RECORDS====================================================
 function edit(uid,uphone,uname)
 {
 	$('#edit').modal('show');
 	$('.edit-name').val(uname);
 	$('.edit-number').val(uphone);
-
-	$('#edit-btn').click(function(){
-
+	$('#edit-btn').click(function()
+	{
 		  $.ajax({
                 url: 'https://joshphonebook.herokuapp.com/updateUser/'+uid,
                 type: 'PATCH',
                 data: {name: $('.edit-name').val(),
                 	   number: $('.edit-number').val(),
                 	      _method: "PATCH"},
-                success: function(res) {
+                success: function(res) 
+                {
                 	
                 	$.alert({
 					    title: 'Alert!',
 					    content: res.message,
 					});
                 	 $("#wew tr").remove();
-					 getrecords();
-
-
-                	 	
+					 getrecords();               	 	
                 }
-        });
-
+         	});
 	});
-
 }
+//==============================================DELETING RECORDS====================================================
 function remove(uid){
 	 $.ajax({
                 url: 'https://joshphonebook.herokuapp.com/delete/'+uid,
-                type: 'delete',
-                
+                type: 'delete',                
                 success: function(res) {
 					$.alert({
 					    title: 'Alert!',
 					    content: res.message,
 					});
                 	 $("#wew tr").remove();
-					 getrecords();
-
-			                          	
+					 getrecords();			                          	
                 }
         });
 }
